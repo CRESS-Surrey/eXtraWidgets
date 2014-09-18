7a716dcbe91a398026bbae7bd9a4ecfa6d02d437
nicolaspayette <nicolaspayette@gmail.com>
2014-09-18 20:34:06 +0100

    add object for GUI strings

---
+++ plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/GUIStrings.scala
@@ -0,0 +1,10 @@
+package uk.ac.surrey.soc.cress.extrawidgets.plugin
+
+object GUIStrings {
+  object Errors {
+    val CantFindToolsMenu = "Can't find NetLogo Tools menu."
+  }
+  object ToolsMenu {
+    val CreateTab = "Create Extra Widgets Tab..."
+  }
+}
