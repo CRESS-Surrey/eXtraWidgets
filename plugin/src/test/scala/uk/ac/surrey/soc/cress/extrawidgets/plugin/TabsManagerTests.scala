880e7446056da3e9e798323e81e4ba609b8381e7
nicolaspayette <nicolaspayette@gmail.com>
2014-09-18 20:37:13 +0100

    test that menu item if correctly added to the app menu
    I'm not really worried about the menu item, but this was a good test case
    to put testing using controlling API in place...

--- plugin/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/TabsManagerTests.scala
+++ plugin/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/TabsManagerTests.scala
@@ -1,17 +1,40 @@
 package uk.ac.surrey.soc.cress.extrawidgets.plugin
 
+import java.beans.PropertyChangeEvent
+import java.beans.PropertyChangeListener
+
+import scala.Array.canBuildFrom
+
 import org.nlogo.app.App
-import org.scalatest.BeforeAndAfterAll
+import org.nlogo.app.ToolsMenu
 import org.scalatest.FunSuite
 
-class TabsManagerTests extends FunSuite with BeforeAndAfterAll {
+import akka.dispatch.Promise
+import javax.swing.JMenuItem
+import uk.ac.surrey.soc.cress.extrawidgets.plugin.util.SwingExecutionContext.swingExecutionContext
 
-  var ewp: ExtraWidgetsPlugin = null
+class TabsManagerTests extends FunSuite with PropertyChangeListener {
 
-  override def beforeAll() {
-    // start NetLogo
-    App.main(Array[String]())
-    ewp = new ExtraWidgetsPlugin(App.app)
+  App.main(Array[String]())
+  val ewp = new ExtraWidgetsPlugin(App.app)
+  val toolsMenu = Promise[ToolsMenu]()
+
+  ewp.addPropertyChangeListener(this)
+
+  def propertyChange(e: PropertyChangeEvent): Unit = {
+    if (e.getPropertyName == "toolsMenu")
+      toolsMenu.success(ewp.toolsMenu)
+  }
+
+  test("'" + GUIStrings.ToolsMenu.CreateTab + "' added to Tools menu") {
+    toolsMenu.onSuccess {
+      case tm: ToolsMenu =>
+        val items = tm.getMenuComponents.collect {
+          case item: JMenuItem if item.getText == GUIStrings.ToolsMenu.CreateTab =>
+            item
+        }
+        assert(items.size === 1)
+    }
   }
 
 }
