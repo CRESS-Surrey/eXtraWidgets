734be9270336b4c7fc623170677b4e64d05f8850
nicolaspayette <nicolaspayette@gmail.com>
2014-09-18 14:54:10 +0100

    remove tab on startup

--- plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/ExtraWidgetsTab.scala
+++ plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/ExtraWidgetsTab.scala
@@ -1,8 +1,12 @@
 package uk.ac.surrey.soc.cress.extrawidgets.plugin
 
 import org.nlogo.app.App
+import org.nlogo.awt.EventQueue.invokeLater
+import org.nlogo.swing.Implicits.thunk2runnable
 
 import javax.swing.JPanel
 
 class ExtraWidgetsTab(app: App) extends JPanel {
+  val tabsManager = new TabsManager(app.tabs)
+  invokeLater { () ⇒ tabsManager.removeTab(this) }
 }
---
+++ plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/TabsManager.scala
@@ -0,0 +1,16 @@
+package uk.ac.surrey.soc.cress.extrawidgets.plugin
+
+import java.awt.Component
+
+import org.nlogo.app.Tabs
+
+class TabsManager(tabs: Tabs) {
+
+  def removeTab(component: Component): Unit =
+    (0 until tabs.getTabCount)
+      .find(i ⇒ tabs.getComponentAt(i) == component)
+      .foreach { i ⇒
+        tabs.remove(component)
+        tabs.removeMenuItem(i)
+      }
+}
