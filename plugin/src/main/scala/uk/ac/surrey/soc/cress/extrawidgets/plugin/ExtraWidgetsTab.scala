2a07b7e5296a67dee773b8e4f7e6b3b9694013a7
nicolaspayette <nicolaspayette@gmail.com>
2014-09-18 20:36:03 +0100

    add "Create extra widget tab" menu item to tools menu

--- plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/ExtraWidgetsTab.scala
+++ plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/ExtraWidgetsTab.scala
@@ -1,12 +1,36 @@
 package uk.ac.surrey.soc.cress.extrawidgets.plugin
 
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
+
 import org.nlogo.app.App
-import org.nlogo.awt.EventQueue.invokeLater
-import org.nlogo.swing.Implicits.thunk2runnable
+import org.nlogo.app.ToolsMenu
 
 import javax.swing.JPanel
 
 class ExtraWidgetsPlugin(app: App) extends JPanel {
-  val tabsManager = new TabsManager(app.tabs)
-  invokeLater { () ⇒ tabsManager.removeTab(this) }
+  self =>
+
+  private var _tabsManager: TabsManager = null
+  def tabsManager = _tabsManager
+  private var _toolsMenu: ToolsMenu = null
+  def toolsMenu = _toolsMenu
+
+  app.frame.addComponentListener(new ComponentAdapter() {
+    override def componentShown(e: ComponentEvent) {
+      val jMenuBar = app.frame.getJMenuBar
+      _toolsMenu =
+        (0 until jMenuBar.getMenuCount)
+          .map(jMenuBar.getMenu)
+          .collect { case m: ToolsMenu => m }
+          .headOption
+          .getOrElse(throw new IllegalStateException(GUIStrings.Errors.CantFindToolsMenu))
+      firePropertyChange("toolsMenu", null, _toolsMenu)
+      toolsMenu.addSeparator()
+      toolsMenu.addMenuItem(GUIStrings.ToolsMenu.CreateTab, 'X', true)
+      _tabsManager = new TabsManager(app.tabs)
+      firePropertyChange("tabsManager", null, _tabsManager)
+      tabsManager.removeTab(self)
+    }
+  })
 }
