4b6c5caf6e1121b85975a8ddf23aa11046a72057
nicolaspayette <nicolaspayette@gmail.com>
2014-09-19 19:13:07 +0100

    use FunSpec and add new tabs tests

--- plugin/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/TabsManagerTests.scala
+++ plugin/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/TabsManagerTests.scala
@@ -2,23 +2,48 @@ package uk.ac.surrey.soc.cress.extrawidgets.plugin
 
 import scala.Array.canBuildFrom
 
-import org.nlogo.app.App
-import org.nlogo.app.ToolsMenu
-import org.scalatest.FunSuite
+import org.scalatest.FunSpec
+import org.scalatest.GivenWhenThen
 import org.scalatest.matchers.ShouldMatchers
 
+import GUIStrings.TabsManager.TabNameMustBeNonEmpty
+import GUIStrings.TabsManager.TabNameMustBeUnique
+import akka.dispatch.Await
+import akka.util.duration.intToDurationInt
 import javax.swing.JMenuItem
 import util.NetLogoInitializer.extraWidgetsPlugin
 
-class TabsManagerTests extends FunSuite with ShouldMatchers {
+class TabsManagerTests extends FunSpec with ShouldMatchers with GivenWhenThen {
 
-  test("'" + GUIStrings.ToolsMenu.CreateTab + "' added to Tools menu") {
-    for (ewp ← extraWidgetsPlugin) {
-      val items = ewp.toolsMenu.getMenuComponents.collect {
-        case item: JMenuItem if item.getText == GUIStrings.ToolsMenu.CreateTab ⇒
-          item
+  describe("The TabsManager") {
+
+    val ewp = Await.result(extraWidgetsPlugin, 30 seconds)
+    val tm = ewp.tabsManager
+
+    it("should add '" + GUIStrings.ToolsMenu.CreateTab + "' to Tools menu") {
+      for (ewp ← extraWidgetsPlugin) {
+        val items = ewp.toolsMenu.getMenuComponents.collect {
+          case item: JMenuItem if item.getText == GUIStrings.ToolsMenu.CreateTab ⇒
+            item
+        }
+        items should have size 1
       }
-      items should have size 1
+    }
+
+    it("should be able to create extra widgets tabs") {
+      tm.addTab("first tab") should be('right)
+    }
+
+    it("should not able to create tabs with duplicate names") {
+      tm.addTab("first tab") shouldEqual Left(TabNameMustBeUnique)
+    }
+
+    it("should not be able to create tabs with empty names") {
+      tm.addTab("") shouldEqual Left(TabNameMustBeNonEmpty)
+    }
+
+    it("should be able to create a second tab with a different name") {
+      tm.addTab("second tab") should be('right)
     }
   }
 
