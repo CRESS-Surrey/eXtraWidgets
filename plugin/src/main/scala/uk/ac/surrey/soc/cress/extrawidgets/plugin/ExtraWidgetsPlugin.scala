4a4b2026cb71db790d580ff0750cf855138fb338
nicolaspayette <nicolaspayette@gmail.com>
2014-09-19 10:32:50 +0100

    get ToolsMenu from pico

--- plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/ExtraWidgetsPlugin.scala
+++ plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/ExtraWidgetsPlugin.scala
@@ -8,23 +8,13 @@ import org.nlogo.app.ToolsMenu
 
 import javax.swing.JPanel
 
-class ExtraWidgetsPlugin(app: App) extends JPanel {
+class ExtraWidgetsPlugin(app: App, toolsMenu: ToolsMenu) extends JPanel {
   self =>
 
   val tabsManager: TabsManager = new TabsManager(app.tabs)
-  private var _toolsMenu: ToolsMenu = null
-  def toolsMenu = _toolsMenu
 
   app.frame.addComponentListener(new ComponentAdapter() {
     override def componentShown(e: ComponentEvent) {
-      val jMenuBar = app.frame.getJMenuBar
-      _toolsMenu =
-        (0 until jMenuBar.getMenuCount)
-          .map(jMenuBar.getMenu)
-          .collect { case m: ToolsMenu => m }
-          .headOption
-          .getOrElse(throw new IllegalStateException(GUIStrings.Errors.CantFindToolsMenu))
-      firePropertyChange("toolsMenu", null, _toolsMenu)
       toolsMenu.addSeparator()
       toolsMenu.addMenuItem(GUIStrings.ToolsMenu.CreateTab, 'X', true)
       tabsManager.removeTab(self)
