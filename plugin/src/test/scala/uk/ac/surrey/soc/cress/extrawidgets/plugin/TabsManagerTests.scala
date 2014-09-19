d8544a339905cb86158111a341aedf00e23addb6
nicolaspayette <nicolaspayette@gmail.com>
2014-09-19 14:36:53 +0100

    test added menu item with extraWidgetsPlugin as Future

--- plugin/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/TabsManagerTests.scala
+++ plugin/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/TabsManagerTests.scala
@@ -8,26 +8,18 @@ import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 
 import javax.swing.JMenuItem
-import util.NetLogoInitializer.app
+import util.NetLogoInitializer.extraWidgetsPlugin
 
 class TabsManagerTests extends FunSuite with ShouldMatchers {
 
   test("'" + GUIStrings.ToolsMenu.CreateTab + "' added to Tools menu") {
-    app.onSuccess {
-      case app: App =>
-        val jMenuBar = app.frame.getJMenuBar
-        val toolsMenu =
-          (0 until jMenuBar.getMenuCount)
-            .map(jMenuBar.getMenu)
-            .collect { case m: ToolsMenu => m }
-            .headOption
-            .getOrElse(throw new IllegalStateException("Can't find tools menu"))
-
-        val items = toolsMenu.getMenuComponents.collect {
-          case item: JMenuItem if item.getText == GUIStrings.ToolsMenu.CreateTab =>
-            item
-        }
-        items should have size 1
+    for (ewp ← extraWidgetsPlugin) {
+      val items = ewp.toolsMenu.getMenuComponents.collect {
+        case item: JMenuItem if item.getText == GUIStrings.ToolsMenu.CreateTab ⇒
+          item
+      }
+      items should have size 1
     }
   }
+
 }
