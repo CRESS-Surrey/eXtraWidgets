017fb199f2790640910685262b9483a99af79369
nicolaspayette <nicolaspayette@gmail.com>
2014-09-19 14:33:11 +0100

    don't load plugin *as* plugin when testing

--- plugin/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/util/NetLogoInitializer.scala
+++ plugin/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/util/NetLogoInitializer.scala
@@ -1,19 +1,34 @@
 package uk.ac.surrey.soc.cress.extrawidgets.plugin.util
 
-import java.awt.event.ComponentAdapter
-import java.awt.event.ComponentEvent
-
 import org.nlogo.app.App
-
 import akka.dispatch.Promise
+import uk.ac.surrey.soc.cress.extrawidgets.plugin.util.Swing.enrichComponent
+import uk.ac.surrey.soc.cress.extrawidgets.plugin.ExtraWidgetsPlugin
+import uk.ac.surrey.soc.cress.extrawidgets.plugin.ExtraWidgetsPlugin
+import org.nlogo.app.ToolsMenu
+import org.nlogo.app.AppFrame
+import javax.swing.JFrame
 
 object NetLogoInitializer {
   import SwingExecutionContext.swingExecutionContext
-  val app = Promise[App]()
+  val extraWidgetsPlugin = Promise[ExtraWidgetsPlugin]()
   App.main(Array[String]())
-  App.app.frame.addComponentListener(new ComponentAdapter() {
-    override def componentShown(e: ComponentEvent) {
-      app.success(App.app)
-    }
-  })
+  App.app.frame.onComponentShown { e ⇒
+    extraWidgetsPlugin.success(
+      new ExtraWidgetsPlugin(
+        App.app.frame,
+        App.app.tabs,
+        getToolsMenu(App.app.frame)
+      )
+    )
+  }
+
+  def getToolsMenu(frame: JFrame) = {
+    val jMenuBar = frame.getJMenuBar
+    (0 until jMenuBar.getMenuCount)
+      .map(jMenuBar.getMenu)
+      .collect { case m: ToolsMenu ⇒ m }
+      .headOption
+      .getOrElse(throw new IllegalStateException("Can't find tools menu"))
+  }
 }
