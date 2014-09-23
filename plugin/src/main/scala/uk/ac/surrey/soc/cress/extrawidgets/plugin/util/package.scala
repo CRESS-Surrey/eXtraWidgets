6e8c8c50353c6287e58a7d237c20af1ab64ee263
nicolaspayette <nicolaspayette@gmail.com>
2014-09-23 15:31:19 +0100

    add RightBiasedEither implicit

--- plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/util/package.scala
+++ plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/util/package.scala
@@ -1,4 +1,12 @@
 package uk.ac.surrey.soc.cress.extrawidgets.plugin
 
 package object util {
+  // see: https://issues.scala-lang.org/browse/SI-5793
+  implicit def eitherToRightBiased[A, B](e: Either[A, B]) =
+    new RightBiasedEither(e)
+  class RightBiasedEither[A, B](val e: Either[A, B]) {
+    def foreach[U](f: B ⇒ U): Unit = e.right.foreach(f)
+    def map[C](f: B ⇒ C): Either[A, C] = e.right.map(f)
+    def flatMap[C](f: B ⇒ Either[A, C]) = e.right.flatMap(f)
+  }
 }
