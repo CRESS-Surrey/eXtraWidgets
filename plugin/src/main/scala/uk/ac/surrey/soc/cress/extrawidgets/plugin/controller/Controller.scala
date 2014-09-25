d0c478dff0b2741266803380ab476b5f66448b9c
nicolaspayette <nicolaspayette@gmail.com>
2014-09-25 17:35:42 +0100

    remove unused import

--- plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/controller/Controller.scala
+++ plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/controller/Controller.scala
@@ -1,7 +1,6 @@
 package uk.ac.surrey.soc.cress.extrawidgets.plugin.controller
 
 import uk.ac.surrey.soc.cress.extrawidgets.plugin.model.Writer
-import uk.ac.surrey.soc.cress.extrawidgets.plugin.view.View
 
 class Controller(writer: Writer) {
   def addTab(id: String): Either[String, Unit] = {
