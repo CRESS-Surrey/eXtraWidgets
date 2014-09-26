42ecd114f2fe2ac71b4f7a92887344c68589c47e
nicolaspayette <nicolaspayette@gmail.com>
2014-09-26 19:31:49 +0100

    adjust import after moving ExtraWidget trait

--- plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/view/ExtraWidgetsTab.scala
+++ plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/view/ExtraWidgetsTab.scala
@@ -1,7 +1,7 @@
 package uk.ac.surrey.soc.cress.extrawidgets.plugin.view
 
 import javax.swing.JPanel
-import uk.ac.surrey.soc.cress.extrawidgets.plugin.gui.ExtraWidget
+import uk.ac.surrey.soc.cress.extrawidgets.plugin.ExtraWidget
 
 class ExtraWidgetsTab(
   val id: String,
