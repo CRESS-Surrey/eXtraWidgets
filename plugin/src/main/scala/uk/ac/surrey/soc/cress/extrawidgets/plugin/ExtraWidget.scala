7a94f17e445f90a3a8c15d9f34a9d6f68c209a28
nicolaspayette <nicolaspayette@gmail.com>
2014-09-26 19:30:45 +0100

    move ExtraWidget trait to main package

---
+++ plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/ExtraWidget.scala
@@ -0,0 +1,8 @@
+package uk.ac.surrey.soc.cress.extrawidgets.plugin
+
+import uk.ac.surrey.soc.cress.extrawidgets.plugin.model.WidgetID
+import uk.ac.surrey.soc.cress.extrawidgets.plugin.model.WidgetID
+
+trait ExtraWidget {
+  val id: WidgetID
+}
--- plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/gui/ExtraWidget.scala
+++
@@ -1,7 +0,0 @@
-package uk.ac.surrey.soc.cress.extrawidgets.plugin.gui
-
-import uk.ac.surrey.soc.cress.extrawidgets.plugin.model.WidgetID
-
-trait ExtraWidget {
-  val id: WidgetID
-}
