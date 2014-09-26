603ffc9d1bc76cdec4f87b5ca32ebfd5e497ad45
nicolaspayette <nicolaspayette@gmail.com>
2014-09-26 11:57:32 +0100

    restructure tests and add failing test for added tabs menu item

--- plugin/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/controller/ControllerTests.scala
+++ plugin/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/controller/ControllerTests.scala
@@ -3,10 +3,10 @@ package uk.ac.surrey.soc.cress.extrawidgets.plugin.controller
 import org.scalatest.FunSpec
 import org.scalatest.GivenWhenThen
 import org.scalatest.matchers.ShouldMatchers
-
 import uk.ac.surrey.soc.cress.extrawidgets.plugin.NetLogoInitializer.extraWidgetsPlugin
 import uk.ac.surrey.soc.cress.extrawidgets.plugin.model.Strings.propertyMustBeNonEmpty
 import uk.ac.surrey.soc.cress.extrawidgets.plugin.model.Strings.propertyMustBeUnique
+import org.nlogo.app.App
 
 class ControllerTests extends FunSpec with ShouldMatchers with GivenWhenThen {
 
@@ -16,32 +16,47 @@ class ControllerTests extends FunSpec with ShouldMatchers with GivenWhenThen {
     val reader = extraWidgetsPlugin.reader
     val gui = extraWidgetsPlugin.gui
 
+    def tabsMenuItemsText = {
+      val menuBar = App.app.frame.getJMenuBar
+      val menus = 0 until menuBar.getMenuCount map menuBar.getMenu
+      for {
+        tabsMenu ← menus.find(_.getText == "Tabs").toSeq
+        item ← 0 until tabsMenu.getItemCount map tabsMenu.getItem
+      } yield item.getText
+    }
+
+    def shouldBeThere(tabID: String) {
+      reader.widgetMap should contain key tabID
+      gui.makeWidgetsMap should contain key tabID
+      tabsMenuItemsText should contain(tabID)
+    }
+
+    def shouldNotBeThere(tabID: String) {
+      reader.widgetMap.get(tabID) should be('empty)
+      gui.makeWidgetsMap.get(tabID) should be('empty)
+      tabsMenuItemsText should (not contain tabID)
+    }
+
     it("should be able to create extra widgets tabs") {
       controller.addTab("first tab") should be('right)
-      reader.widgetMap.get("first tab") should not be 'empty
-      gui.makeWidgetsMap.get("first tab") should not be 'empty
+      shouldBeThere("first tab")
     }
 
     it("should not able to create tabs with duplicate id") {
       controller.addTab("first tab") shouldEqual Left(propertyMustBeUnique("id", "first tab"))
-      reader.widgetMap.get("first tab") should not be 'empty
-      gui.makeWidgetsMap.get("first tab") should not be 'empty
+      shouldBeThere("first tab")
     }
 
     it("should not be able to create tabs with empty id") {
       controller.addTab("") shouldEqual Left(propertyMustBeNonEmpty("id"))
-      reader.widgetMap.get("first tab") should not be 'empty
-      gui.makeWidgetsMap.get("first tab") should not be 'empty
-      reader.widgetMap.get("") should be('empty)
-      gui.makeWidgetsMap.get("") should be('empty)
+      shouldBeThere("first tab")
+      shouldNotBeThere("")
     }
 
     it("should be able to create a second tab with a different id") {
       controller.addTab("second tab") should be('right)
-      reader.widgetMap.get("first tab") should not be 'empty
-      gui.makeWidgetsMap.get("first tab") should not be 'empty
-      reader.widgetMap.get("second tab") should not be 'empty
-      gui.makeWidgetsMap.get("second tab") should not be 'empty
+      shouldBeThere("first tab")
+      shouldBeThere("second tab")
     }
   }
 }
