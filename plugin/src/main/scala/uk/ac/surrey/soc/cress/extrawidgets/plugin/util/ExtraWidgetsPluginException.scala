a4cf3e7ae52818673c300298b46b80f73e01cbd9
nicolaspayette <nicolaspayette@gmail.com>
2014-09-30 12:44:59 +0100

    give shorter name to custom Exception

--- plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/util/ExtraWidgetsPluginException.scala
+++ plugin/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/plugin/util/ExtraWidgetsPluginException.scala
@@ -1,6 +1,6 @@
 package uk.ac.surrey.soc.cress.extrawidgets.plugin.util
 
-class ExtraWidgetsPluginException(message: String, cause: Throwable = null)
+case class XWException(message: String, cause: Throwable = null)
   extends Exception(message, cause) {
   if (cause != null) setStackTrace(cause.getStackTrace)
 }
