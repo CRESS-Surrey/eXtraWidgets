0709ade19607dd069e1dbb709b1a8de995c0bf52
Nicolas Payette <nicolaspayette@gmail.com>
2014-10-08 11:05:12 +0100

    fix tests for uppercase keys (close #55)

--- core/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/core/GUITests.scala
+++ core/src/test/scala/uk/ac/surrey/soc/cress/extrawidgets/core/GUITests.scala
@@ -2,13 +2,16 @@ package uk.ac.surrey.soc.cress.extrawidgets.core
 
 import scala.Array.canBuildFrom
 import scala.Option.option2Iterable
+
 import org.nlogo.app.App
 import org.scalatest.FunSpec
 import org.scalatest.GivenWhenThen
 import org.scalatest.matchers.ShouldMatchers
+
+import NetLogoInitializer.extraWidgetsManager
 import Strings.CreateTab
 import javax.swing.JMenuItem
-import NetLogoInitializer.extraWidgetsManager
+import uk.ac.surrey.soc.cress.extrawidgets.api.XWException
 import uk.ac.surrey.soc.cress.extrawidgets.state.Strings.propertyMustBeNonEmpty
 import uk.ac.surrey.soc.cress.extrawidgets.state.Strings.propertyMustBeUnique
 
@@ -50,37 +53,39 @@ class GUITests extends FunSpec with ShouldMatchers with GivenWhenThen {
 
     it("should be able to create extra widgets tabs") {
       gui.addTab("first tab") should be('right)
-      shouldBeThere("first tab")
+      shouldBeThere("FIRST TAB")
     }
 
     it("should not able to create tabs with duplicate id") {
-      gui.addTab("first tab") shouldEqual Left(propertyMustBeUnique("widget key", "first tab"))
-      shouldBeThere("first tab")
+      gui.addTab("first tab") shouldEqual
+        Left(XWException(propertyMustBeUnique("widget key", "FIRST TAB")))
+      shouldBeThere("FIRST TAB")
     }
 
     it("should not be able to create tabs with empty id") {
-      gui.addTab("") shouldEqual Left(propertyMustBeNonEmpty("widget key"))
-      shouldBeThere("first tab")
+      gui.addTab("") shouldEqual
+        Left(XWException(propertyMustBeNonEmpty("widget key")))
+      shouldBeThere("FIRST TAB")
       shouldNotBeThere("")
     }
 
     it("should be able to create a second tab with a different id") {
       gui.addTab("second tab") should be('right)
-      shouldBeThere("first tab")
-      shouldBeThere("second tab")
+      shouldBeThere("FIRST TAB")
+      shouldBeThere("SECOND TAB")
     }
 
     it("should be able to remove the first tab while keeping the second one") {
       gui.removeTab("first tab")
-      shouldNotBeThere("first tab")
-      shouldBeThere("second tab")
+      shouldNotBeThere("FIRST TAB")
+      shouldBeThere("SECOND TAB")
     }
 
     it("should then be able to add a third tab") {
       gui.addTab("third tab")
-      shouldNotBeThere("first tab")
-      shouldBeThere("second tab")
-      shouldBeThere("third tab")
+      shouldNotBeThere("FIRST TAB")
+      shouldBeThere("SECOND TAB")
+      shouldBeThere("THIRD TAB")
     }
 
   }
