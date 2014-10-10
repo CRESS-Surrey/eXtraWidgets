f11da60851ab1877bc310ff5733e829c34a2fc9d
Nicolas Payette <nicolaspayette@gmail.com>
2014-10-10 18:46:47 +0100

    add JComponentWidgetTrait, with only wxOpaque for now

---
+++ api/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/api/JComponentWidget.scala
@@ -0,0 +1,8 @@
+package uk.ac.surrey.soc.cress.extrawidgets.api
+
+import javax.swing.JComponent
+
+trait JComponentWidget extends ComponentWidget {
+  self: JComponent ⇒
+  val xwOpaque = new BooleanPropertyDef(this, (b) ⇒ setOpaque(b), isOpaque)
+}
--- plugin/widgets/NoteWidget/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/note/Note.scala
+++ plugin/widgets/NoteWidget/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/note/Note.scala
@@ -3,13 +3,13 @@ package uk.ac.surrey.soc.cress.extrawidgets.note
 import org.nlogo.window.GUIWorkspace
 
 import javax.swing.JLabel
-import uk.ac.surrey.soc.cress.extrawidgets.api.ComponentWidget
+import uk.ac.surrey.soc.cress.extrawidgets.api.JComponentWidget
 import uk.ac.surrey.soc.cress.extrawidgets.api.PropertyMap
 import uk.ac.surrey.soc.cress.extrawidgets.api.StringPropertyDef
 import uk.ac.surrey.soc.cress.extrawidgets.api.WidgetKey
 
 class Note(val key: WidgetKey, properties: PropertyMap, ws: GUIWorkspace)
-  extends JLabel with ComponentWidget {
+  extends JLabel with JComponentWidget {
 
   val text = new StringPropertyDef(this, setText, getText, java.lang.Math.random().toString)
 
