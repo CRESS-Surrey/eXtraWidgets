129190d6e5b79238d63683abf26fc0b65dea8ec3
Nicolas Payette <nicolaspayette@gmail.com>
2014-10-13 15:18:11 +0100

    set tab background to white by default

--- common/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/core/Tab.scala
+++ common/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/core/Tab.scala
@@ -1,16 +1,17 @@
 package uk.ac.surrey.soc.cress.extrawidgets.gui
 
+import java.awt.Color.white
 import org.nlogo.app.App
 import org.nlogo.app.AppFrame
 import org.nlogo.swing.RichAction
 import org.nlogo.window.GUIWorkspace
-
 import javax.swing.JPanel
 import uk.ac.surrey.soc.cress.extrawidgets.api.JComponentWidget
 import uk.ac.surrey.soc.cress.extrawidgets.api.PropertyMap
 import uk.ac.surrey.soc.cress.extrawidgets.api.StringPropertyDef
 import uk.ac.surrey.soc.cress.extrawidgets.api.WidgetKey
 import uk.ac.surrey.soc.cress.extrawidgets.api.XWException
+import uk.ac.surrey.soc.cress.extrawidgets.api.ColorPropertyDef
 
 class Tab(
   val key: WidgetKey,
@@ -20,6 +21,7 @@ class Tab(
   with JComponentWidget {
 
   val xwTitle = new StringPropertyDef(this, v ⇒ setTitle(v), () ⇒ getTitle, () ⇒ key)
+  override val xwBackground = new ColorPropertyDef(this, setBackground, getBackground, () ⇒ white)
 
   val tabs = ws.getFrame.asInstanceOf[AppFrame].getLinkChildren
     .collectFirst { case app: App ⇒ app.tabs }
