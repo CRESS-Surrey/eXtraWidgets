e939cef0bd7cf109551ca6bcc519f15b8912e2a4
Nicolas Payette <nicolaspayette@gmail.com>
2014-10-17 12:46:41 +0100

    deal with opacity and borders in JComponentWidget

--- api/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/api/JComponentWidget.scala
+++ api/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/api/JComponentWidget.scala
@@ -1,8 +1,31 @@
 package uk.ac.surrey.soc.cress.extrawidgets.api
 
+import org.nlogo.swing.Utils.createWidgetBorder
+
+import javax.swing.BorderFactory.createCompoundBorder
+import javax.swing.BorderFactory.createEmptyBorder
 import javax.swing.JComponent
 
 trait JComponentWidget extends ComponentWidget {
   self: JComponent ⇒
-  val xwOpaque = new BooleanPropertyDef(this, (b) ⇒ setOpaque(b), isOpaque, const(isOpaque))
+
+  private val padding = createEmptyBorder(1, 1, 1, 1)
+  private val _borderWhenOpaque =
+    createCompoundBorder(createWidgetBorder, padding)
+  def borderWhenOpaque = _borderWhenOpaque
+  private val _borderWhenTransparent =
+    createCompoundBorder(createEmptyBorder(3, 3, 2, 2), padding)
+  def borderWhenTransparent = _borderWhenTransparent
+
+  def updateBorder() =
+    if (isOpaque) setBorder(borderWhenOpaque)
+    else setBorder(borderWhenTransparent)
+
+  private val _defaultOpacity = isOpaque
+  def defaultOpacity = _defaultOpacity
+  val xwOpaque = new BooleanPropertyDef(this,
+    (b) ⇒ { setOpaque(b); updateBorder() },
+    isOpaque,
+    () ⇒ defaultOpacity)
+
 }
