eafbdff9d7b3dd30bb97bbe6770cbc65e8f6fc0c
Nicolas Payette <nicolaspayette@gmail.com>
2014-10-17 12:50:10 +0100

    remove unused imports

--- xw/widgets/CheckboxWidget/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/checkbox/Checkbox.scala
+++ xw/widgets/CheckboxWidget/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/checkbox/Checkbox.scala
@@ -4,12 +4,9 @@ import org.nlogo.window.GUIWorkspace
 import org.nlogo.window.InterfaceColors.SWITCH_BACKGROUND
 
 import javax.swing.JCheckBox
-import uk.ac.surrey.soc.cress.extrawidgets.api.BooleanPropertyDef
-import uk.ac.surrey.soc.cress.extrawidgets.api.JComponentWidget
+import uk.ac.surrey.soc.cress.extrawidgets.api.AbstractButtonWidget
 import uk.ac.surrey.soc.cress.extrawidgets.api.PropertyMap
 import uk.ac.surrey.soc.cress.extrawidgets.api.WidgetKey
-import uk.ac.surrey.soc.cress.extrawidgets.api.const
-import uk.ac.surrey.soc.cress.extrawidgets.api.AbstractButtonWidget
 
 class Checkbox(val key: WidgetKey, properties: PropertyMap, ws: GUIWorkspace)
   extends JCheckBox with AbstractButtonWidget {
