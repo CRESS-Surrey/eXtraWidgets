faa08c2b3d613c15200834fbfdc0697f6da05918
Nicolas Payette <nicolaspayette@gmail.com>
2014-10-17 13:19:22 +0100

    put some meat on slider, close #80, close #81, close #82

--- xw/widgets/SliderWidget/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/slider/Slider.scala
+++ xw/widgets/SliderWidget/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/slider/Slider.scala
@@ -1,13 +1,70 @@
 package uk.ac.surrey.soc.cress.extrawidgets.slider
 
-import org.nlogo.window.GUIWorkspace
+import java.awt.BorderLayout
+import java.awt.BorderLayout.CENTER
+import java.awt.BorderLayout.LINE_END
+import java.awt.BorderLayout.PAGE_START
 
+import org.nlogo.window.GUIWorkspace
+import org.nlogo.window.InterfaceColors.SLIDER_BACKGROUND
+
+import javax.swing.BorderFactory.createEmptyBorder
+import javax.swing.JLabel
+import javax.swing.JPanel
 import javax.swing.JSlider
+import javax.swing.SwingConstants
+import uk.ac.surrey.soc.cress.extrawidgets.api.IntegerPropertyDef
 import uk.ac.surrey.soc.cress.extrawidgets.api.JComponentWidget
 import uk.ac.surrey.soc.cress.extrawidgets.api.PropertyMap
+import uk.ac.surrey.soc.cress.extrawidgets.api.StringPropertyDef
 import uk.ac.surrey.soc.cress.extrawidgets.api.WidgetKey
+import uk.ac.surrey.soc.cress.extrawidgets.api.const
+import uk.ac.surrey.soc.cress.extrawidgets.api.swing._
 
 class Slider(val key: WidgetKey, properties: PropertyMap, ws: GUIWorkspace)
-  extends JSlider with JComponentWidget {
+  extends JPanel with JComponentWidget {
+
+  setLayout(new BorderLayout())
+
+  private val _borderPadding = createEmptyBorder(0, 4, 0, 4)
+  override def borderWhenOpaque = _borderPadding
+
+  private var text = key
+  val slider = new JSlider()
+  val textLabel = new JLabel(text) {
+    setVerticalAlignment(SwingConstants.TOP)
+  }
+  val valueLabel = new JLabel(slider.getValue.toString) {
+    setVerticalAlignment(SwingConstants.TOP)
+  }
+  add(slider, PAGE_START)
+  add(textLabel, CENTER)
+  add(valueLabel, LINE_END)
+
+  override def defaultBackground = SLIDER_BACKGROUND
+
+  val xwText = new StringPropertyDef(this,
+    x ⇒ { text = x; textLabel.setText(text) },
+    () ⇒ text,
+    const(text))
+
+  val xwMinimum = new IntegerPropertyDef(this,
+    x ⇒ slider.setMinimum(x),
+    slider.getMinimum,
+    const(slider.getMinimum))
+
+  val xwMaximum = new IntegerPropertyDef(this,
+    x ⇒ slider.setMaximum(x),
+    slider.getMaximum,
+    const(slider.getMaximum))
+
+  val xwValue = new IntegerPropertyDef(this,
+    x ⇒ slider.setValue(x),
+    slider.getValue,
+    const(slider.getValue))
+
+  slider.onChange { _ ⇒
+    valueLabel.setText(slider.getValue.toString)
+  }
 
 }
