c23a25f689d93519f9aabbbf62f0b287f45f7759
Nicolas Payette <nicolaspayette@gmail.com>
2014-11-06 18:20:20 +0000

    add (previously forgotten) default property to checkbox

--- xw/widgets/CheckboxWidget/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/checkbox/Checkbox.scala
+++ xw/widgets/CheckboxWidget/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/checkbox/Checkbox.scala
@@ -2,12 +2,13 @@ package uk.ac.surrey.soc.cress.extrawidgets.checkbox
 
 import org.nlogo.window.GUIWorkspace
 import org.nlogo.window.InterfaceColors.SWITCH_BACKGROUND
-
 import javax.swing.JCheckBox
 import uk.ac.surrey.soc.cress.extrawidgets.api.AbstractButtonWidget
 import uk.ac.surrey.soc.cress.extrawidgets.api.StateUpdater
 import uk.ac.surrey.soc.cress.extrawidgets.api.WidgetKey
+import uk.ac.surrey.soc.cress.extrawidgets.api.annotations.DefaultProperty
 
+@DefaultProperty("SELECTED?")
 class Checkbox(
   val key: WidgetKey,
   val stateUpdater: StateUpdater,
