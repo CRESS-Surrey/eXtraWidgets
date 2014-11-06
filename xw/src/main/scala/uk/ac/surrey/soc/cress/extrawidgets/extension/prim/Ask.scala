8d3775dcda85b0ab3c2dedee395f80fc511d214c
Nicolas Payette <nicolaspayette@gmail.com>
2014-11-06 16:53:09 +0000

    make xw:ask work with lists (#45)

--- xw/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/extension/prim/Ask.scala
+++ xw/src/main/scala/uk/ac/surrey/soc/cress/extrawidgets/extension/prim/Ask.scala
@@ -1,22 +1,33 @@
 package uk.ac.surrey.soc.cress.extrawidgets.extension.prim
 
-import org.nlogo.api.Argument
+import org.nlogo.api._
 import org.nlogo.api.Context
 import org.nlogo.api.DefaultCommand
+import org.nlogo.api.LogoList
 import org.nlogo.api.Syntax.CommandBlockType
+import org.nlogo.api.Syntax.ListType
 import org.nlogo.api.Syntax.StringType
 import org.nlogo.api.Syntax.commandSyntax
-
 import uk.ac.surrey.soc.cress.extrawidgets.extension.WidgetContextManager
+import uk.ac.surrey.soc.cress.extrawidgets.api._
 
 class Ask(wcm: WidgetContextManager)
   extends DefaultCommand
   with HasCommandBlock {
   override def getSyntax = commandSyntax(Array(
-    StringType, CommandBlockType
+    StringType | ListType, CommandBlockType
   ))
+  private def runFor(key: WidgetKey, context: Context) =
+    wcm.withContext(key) { () ⇒ runBlock(context) }
   def perform(args: Array[Argument], context: Context): Unit =
-    wcm.withContext(args(0).getString) { () ⇒
-      runBlock(context)
+    args(0).get match {
+      case key: String ⇒ runFor(key, context)
+      case list: LogoList ⇒
+        for (obj ← list) obj match {
+          case key: String ⇒ runFor(key, context)
+          case _ ⇒ throw XWException(
+            "Expected a widget key string but got " + Dump.logoObject(obj) +
+              " of type " + Dump.typeName(obj).toUpperCase + " instead.")
+        }
     }
 }
