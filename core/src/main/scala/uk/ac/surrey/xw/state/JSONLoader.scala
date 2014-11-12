42a94a1cad5ceabd8143a4921d6315ee12954811
Nicolas Payette <nicolaspayette@gmail.com>
2014-11-12 14:51:32 +0000

    isolate JSONLoader from Writer

---
+++ core/src/main/scala/uk/ac/surrey/xw/state/JSONLoader.scala
@@ -0,0 +1,49 @@
+package uk.ac.surrey.xw.state
+
+import scala.collection.JavaConverters.asScalaBufferConverter
+import scala.collection.JavaConverters.mapAsScalaMapConverter
+
+import org.json.simple.parser.JSONParser
+import org.json.simple.parser.ParseException
+import org.nlogo.api.Dump
+import org.nlogo.api.LogoList
+
+import uk.ac.surrey.xw.api.XWException
+
+class JSONLoader(writer:Writer) {
+  /**
+   * Handle the possible values returned by the JSON
+   * parser. Nulls are unsupported for now, and so are
+   * other custom object that could, in theory, be
+   * add (e.g. nobody, extension objects, etc.)
+   */
+  def convertJSONValue(v: Any): AnyRef = try v match {
+    case l: java.util.List[_] ⇒
+      LogoList(l.asScala.map(convertJSONValue): _*)
+    case s: java.lang.String ⇒ s
+    case n: java.lang.Number ⇒ Double.box(n.doubleValue)
+    case b: java.lang.Boolean ⇒ b
+  } catch {
+    case e: MatchError ⇒ throw XWException(
+      "Unsupported value in JSON input: " +
+        Dump.logoObject(v.asInstanceOf[AnyRef]), e)
+  }
+
+  def load(json: String): Unit = {
+    val widgetMap =
+      try new JSONParser().parse(json).asInstanceOf[java.util.Map[_, _]]
+      catch {
+        case e: ParseException ⇒ throw XWException(
+          "Error parsing JSON input at position " + e.getPosition, e)
+        case e: ClassCastException ⇒ throw XWException(
+          "Error parsing JSON input: main value is not a JSON object.", e)
+      }
+    for {
+      (widgetKey: String, jMap: java.util.Map[_, _]) ← widgetMap.asScala
+      propertyMap = jMap.asScala.map {
+        case (k: String, v) ⇒ k -> convertJSONValue(v)
+      }
+    } writer.add(widgetKey, propertyMap.toMap)
+  }
+
+}
--- xw/src/main/scala/uk/ac/surrey/xw/extension/prim/LoadJSON.scala
+++ xw/src/main/scala/uk/ac/surrey/xw/extension/prim/LoadJSON.scala
@@ -5,12 +5,13 @@ import org.nlogo.api.Context
 import org.nlogo.api.DefaultCommand
 import org.nlogo.api.Syntax.StringType
 import org.nlogo.api.Syntax.commandSyntax
-
 import uk.ac.surrey.xw.state.Writer
+import uk.ac.surrey.xw.state.JSONLoader
 
 class LoadJSON(writer: Writer) extends DefaultCommand {
   override def getSyntax = commandSyntax(Array(StringType))
+  val jsonLoader = new JSONLoader(writer)
   def perform(args: Array[Argument], context: Context): Unit = {
-    writer.loadJSON(args(0).getString)
+    jsonLoader.load(args(0).getString)
   }
 }
