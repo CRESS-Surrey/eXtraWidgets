814555f8732bab0b4331c508499be742e16fe656
Nicolas Payette <nicolaspayette@gmail.com>
2014-12-04 11:39:38 +0000

    make notes white/transparent by default

--- xw/widgets/NoteWidget/src/main/scala/uk/ac/surrey/xw/note/Note.scala
+++ xw/widgets/NoteWidget/src/main/scala/uk/ac/surrey/xw/note/Note.scala
@@ -1,8 +1,12 @@
 package uk.ac.surrey.xw.note
 
+import java.awt.Color.white
+
 import org.nlogo.window.GUIWorkspace
 
 import javax.swing.JLabel
+import uk.ac.surrey.xw.api.BooleanProperty
+import uk.ac.surrey.xw.api.ColorProperty
 import uk.ac.surrey.xw.api.JComponentWidget
 import uk.ac.surrey.xw.api.JComponentWidgetKind
 import uk.ac.surrey.xw.api.StateUpdater
@@ -12,6 +16,10 @@ import uk.ac.surrey.xw.api.WidgetKey
 class NoteKind[W <: Note] extends JComponentWidgetKind[W] {
   override val name = "NOTE"
   override val newWidget = new Note(_, _, _)
+  override val colorProperty = new ColorProperty[W](
+    "COLOR", Some(_.setBackground(_)), _.getBackground, white)
+  override val opaqueProperty = new BooleanProperty[W](
+    "OPAQUE", Some((w, b) ⇒ { w.setOpaque(b); w.updateBorder() }), _.isOpaque, false)
   val textProperty = new StringProperty[W]("TEXT", Some(_.setText(_)), _.getText)
   val defaultProperty = Some(textProperty)
   override def propertySet = super.propertySet + textProperty
