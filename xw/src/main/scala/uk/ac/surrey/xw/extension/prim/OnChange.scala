22e226c2ba8d60787a628b2ae945392c30a89028
Bryan Head <bryan.head@gmail.com>
2015-04-21 14:43:26 -0500

    Make event listener jobs concurrent

--- xw/src/main/scala/uk/ac/surrey/xw/extension/prim/OnChange.scala
+++ xw/src/main/scala/uk/ac/surrey/xw/extension/prim/OnChange.scala
@@ -21,18 +21,19 @@ abstract class OnChangePrim(writer: Writer, wcm: WidgetContextManager) extends D
 
   def addListener(context: Context, widgetKey: WidgetKey, propertyKey: PropertyKey): Unit = {
     val extContext = context.asInstanceOf[ExtensionContext]
-    val agentSet = extContext.workspace.world.observers
+    val ws = extContext.workspace
+    val agentSet = ws.world.observers
     // The ip of the block is relative to the ip at *this* point in time. Since the listeners will activate later on,
     // after this Context has changed, the ip will be lost. Thus, we need to compute the block's ip now. This
     // prevents us from using HasCommandBlock to run the blocks. BCH 4/19/2015
-    val ip = extContext.nvmContext.ip + 1
-    extContext.nvmContext.activation.procedure.code(ip)
+    val ip = extContext.nvmContext.ip
     val childContext = new NvmContext(extContext.nvmContext, extContext.workspace.world.observer)
-    childContext.agent = extContext.workspace.world.observer
+    childContext.agent = ws.world.observer
 
-    val listener = ChangeListener { _ => wcm.withContext(widgetKey) { () =>
-      childContext.runExclusiveJob(agentSet, ip)
-    }}
+    val listener = ChangeListener { _ =>
+      childContext.ip = ip
+      ws.addJobFromJobThread(childContext.makeConcurrentJob(agentSet))
+    }
 
     OnChange.listeners.get((widgetKey, propertyKey)).foreach(writer.removeSubscription)
 
