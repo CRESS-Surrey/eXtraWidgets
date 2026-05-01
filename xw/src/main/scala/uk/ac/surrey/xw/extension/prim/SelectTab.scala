e7359d7d8f9bad8c3f4df39378ca81c0e05f769d
Nicolas Payette <nicolaspayette@gmail.com>
2026-05-01 14:43:55 +0100

    Fix GUI tab selection timing

--- xw/src/main/scala/uk/ac/surrey/xw/extension/prim/SelectTab.scala
+++ xw/src/main/scala/uk/ac/surrey/xw/extension/prim/SelectTab.scala
@@ -3,7 +3,7 @@ package uk.ac.surrey.xw.extension.prim
 import org.nlogo.api.Argument
 import org.nlogo.api.Command
 import org.nlogo.api.Context
-import org.nlogo.awt.EventQueue.invokeLater
+import org.nlogo.awt.EventQueue.invokeAndWait
 import org.nlogo.core.Syntax.NumberType
 import org.nlogo.core.Syntax.StringType
 import org.nlogo.core.Syntax.commandSyntax
@@ -22,15 +22,44 @@ class SelectTab(reader: Reader, ws: AbstractWorkspace) extends Command {
       case guiWS: GUIWorkspace =>
         args(0).get match {
           case n: java.lang.Number =>
-            val i = n.intValue - 1
-            if (i < 0 || i >= guiWS.tabs.getTabCount) throw XWException(
-              "Invalid tab index: " + n.intValue + ".")
-            invokeLater { guiWS.tabs.setSelectedIndex(i) }
+            onGUIThread {
+              val i = n.intValue - 1
+              if (i < 0 || i >= guiWS.tabs.getTabCount) throw XWException(
+                "Invalid tab index: " + n.intValue + ".")
+              guiWS.tabs.setSelectedIndex(i)
+            }
           case s: java.lang.String =>
-            val tab = guiWS.xwTabs.find(_.key == s).getOrElse(throw XWException(
-              "Unknown tab key: " + s + "."))
-            invokeLater { guiWS.tabs.setSelectedComponent(tab) }
+            if (!reader.contains(s) || reader.get(reader.kindPropertyKey, s) != reader.tabKindName)
+              throw XWException("Unknown tab key: " + s + ".")
+            onGUIThread {
+              val tab = guiWS.xwTabs.find(_.key == s).getOrElse(throw XWException(
+                "Tab exists in xw state but has not been added to the NetLogo GUI: " + s + "."))
+              guiWS.tabs.setSelectedComponent(tab)
+            }
         }
       case _ => // we're most likely headless, do nothing...
     }
+
+  private def onGUIThread(action: => Unit): Unit = {
+    var failure: Option[Exception] = None
+    try {
+      // Tab creation is queued on the Swing event thread.  Waiting here makes
+      // `xw:create-tab ... xw:select-tab ...` reliable in one NetLogo job,
+      // which is the documented startup use case for this primitive.
+      invokeAndWait {
+        try action
+        catch {
+          case e: Exception => failure = Some(e)
+        }
+      }
+    } catch {
+      case e: InterruptedException =>
+        Thread.currentThread.interrupt()
+        throw XWException("Interrupted while selecting NetLogo tab.", e)
+    }
+    failure.foreach {
+      case e: XWException => throw e
+      case e => throw XWException(e.getMessage, e)
+    }
+  }
 }
